29379a8540233dd4789a55689a123a3133a81e6e
Aleksandr Apanasik <alex.apanasik@gmail.com>
2019-06-02 23:30:47 +0300

    cosmetic changes

--- tenders/views.py
+++ tenders/views.py
@@ -1,13 +1,7 @@
 from .models import Tender
 from .serializers import TenderSerializer
 
-# from rest_framework import viewsets
 from rest_framework import generics
-
-
-# class TenderViewSet(viewsets.ModelViewSet):
-#     queryset = Tender.objects.all()
-#     serializer_class = TenderSerializer
 
 
 class TenderAPIView(generics.ListAPIView):
