bdd841ef2ca21b392ad17ab01c75584b21ee2751
Aleksandr Apanasik <alex.apanasik@gmail.com>
2019-06-03 22:17:31 +0300

    add filtering by list of plus-words

--- tenders/views.py
+++ tenders/views.py
@@ -3,9 +3,24 @@ from .serializers import TenderSerializer
 
 from rest_framework import generics
 
+from datetime import date
+from django.db.models import Q
+
 
 class TenderAPIView(generics.ListAPIView):
     serializer_class = TenderSerializer
-    queryset = Tender.objects.all()
     name = 'All tenders list'
+
+    def get_queryset(self):
+        plus_words = ['сети', 'данных']
+        wanted_items = set()
+
+        for word in plus_words:
+        
+            for item in Tender.objects.filter(Q(exp_date__gte=date.today()),Q(description__icontains=word)):
+                wanted_items.add(item.pk)
+            
+        return Tender.objects.filter(pk__in=wanted_items)
+ 
+
 
